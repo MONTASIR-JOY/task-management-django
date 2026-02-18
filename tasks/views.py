941b155345495ac211b2f8eb9c70217b82308a05
MONTASIR-JOY <180053067+MONTASIR-JOY@users.noreply.github.com>
2026-02-18 19:37:37 +0600

    dashboard feature implemented

--- tasks/views.py
+++ tasks/views.py
@@ -8,3 +8,11 @@ def contact(request):
     return HttpResponse("<h1 style='color: red'>This is contact page</h1>")
 def show_task(request):
     return HttpResponse("This is our task page")
+
+def show_specific_task(request,id):
+    print("id",id)
+    print("id type",type(id))
+    return HttpResponse(f"This is specific task page {id}")
+
+def dashboard(request,id):
+    return HttpResponse("This is dashboard")
